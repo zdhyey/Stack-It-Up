804eaebe71840980dacea5df3344f68458cd7b3b
Dhyey Zala <50908084+zdhyey@users.noreply.github.com>
2020-12-18 11:22:37 +0530

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            if(MovingCube.CurrentCube != null)
+            {
+                MovingCube.CurrentCube.Stop();
+            }
+            FindObjectOfType<Spawner>().SpawnCube();
+        }
+    }
+}
---
+++ Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    
+    [SerializeField] public MovingCube cubePrefab;
+
+    public void SpawnCube()
+    {
+        var cube = Instantiate(cubePrefab);
+    
+        if(MovingCube.LastCube != null && MovingCube.LastCube.gameObject != GameObject.Find("Start"))
+        {
+            cube.transform.position = new Vector3(transform.position.x, 
+            MovingCube.LastCube.transform.position.y + cubePrefab.transform.localScale.y,
+            transform.position.z); 
+        }
+        else
+        {
+            cube.transform.position = transform.position;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, cubePrefab.transform.localScale);
+    }
+}
